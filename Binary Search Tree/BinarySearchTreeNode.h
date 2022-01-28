ac6d51c74141c9a8c8d3ac395656238bbe139733
Aadesh singh <aadeshsingh146@gmail.com>
2022-01-28 16:48:51 +0530

    Binary search tree header file

---
+++ Binary Search Tree/BinarySearchTreeNode.h
@@ -0,0 +1,17 @@
+template<typename T>
+class BST{
+    public:
+    T data;
+    BST* left;
+    BST* right;
+
+    BST(T data){
+        this->data = data;
+        left = NULL;
+        right = NULL;
+    }
+    ~BST(){
+        delete left;
+        delete right;
+    }
+};
