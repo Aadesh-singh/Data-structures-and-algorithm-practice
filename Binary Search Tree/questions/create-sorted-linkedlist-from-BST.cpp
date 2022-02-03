88c774c656cb0940b58e9d97e0aed49534bedf4d
Aadesh singh <aadeshsingh146@gmail.com>
2022-02-03 22:27:36 +0530

    Created Sorted Linked list from BST

--- Binary Search Tree/questions/create-sorted-linkedlist-from-BST.cpp
+++ Binary Search Tree/questions/create-sorted-linkedlist-from-BST.cpp
@@ -68,9 +68,55 @@ class node{
         this->data = data;
         this->next = NULL;
     }
+    ~node(){
+        delete next;
+    }
 };
 
+void findNode(BST<int>* root, int k){
+    if(!root) return;
+    if(root->data == k){
+        root->data = INT_MAX;
+        return;
+    }
+     if(root->left) findNode(root->left, k);
+     if(root->right) findNode(root->right, k);
+}
 
+
+int minimum(BST<int>* root){
+    if(!root) return INT_MAX;
+    int left = minimum(root->left);
+    int right = minimum(root->right);
+    int minimum1 = min(root->data, min(left, right));
+    
+    return minimum1;
+}
+
+int countNode(BST<int>* root){
+    if(!root) return 0;
+    return 1 + countNode(root->left) + countNode(root->right);
+}
+
+node* createSortedLL(BST<int>* root){
+    if(!root) return NULL;
+    node* head = new node(minimum(root));
+    findNode(root, head->data);
+    node* tail = head;
+    int min = minimum(root);
+    findNode(root, min);
+    int i = 1;
+    int count = countNode(root);
+    while(i< count){
+        node* newNode = new node(min);
+        tail->next = newNode;
+        tail = tail->next;
+        min = minimum(root);
+        findNode(root, min);
+        i++;
+    }
+    return head;
+}
 void printLL(node* head){
     node* temp = head;
     while(temp != NULL){
@@ -78,86 +124,12 @@ void printLL(node* head){
         temp = temp->next;
     }
 }
-
-// method 2
-class Pair{
-    public:
-    node* head;
-    node* tail;
-};
-
-Pair createLL_helper(BST<int>* root){
-    if(!root){
-        Pair ans;
-        ans.head = NULL;
-        ans.tail = NULL;
-        return ans;
-    }
-    Pair left = createLL_helper(root->left);
-    Pair right = createLL_helper(root->right);
-    node* curr = new node(root->data);
-    if(left.head != NULL && right.head != NULL){
-        left.tail->next = curr;
-        left.tail = right.tail;
-        curr->next = right.head;
-        return left;
-    } 
-    else if(!left.head && right.head){
-        curr->next = right.head;
-        Pair ans;
-        ans.head = curr;
-        ans.tail = right.tail;
-        return ans;
-    }
-    else if(left.head && !right.head){
-        left.tail->next = curr;
-        left.tail = curr;
-        return left;
-    } 
-    else {
-        Pair ans;
-        ans.head = curr;
-        ans.tail = curr;
-
-        return ans;
-    }
-}
-
-node* createLL(BST<int>* root){
-    return createLL_helper(root).head;
-}
-
-// Method: 1
-node* createSortedLL(BST<int>* root){
-    if(!root) return NULL;
-
-    node* left = createSortedLL(root->left);
-    node* right = createSortedLL(root->right);
-
-    node* temp = left;
-    if(left != NULL){
-        while(temp->next != NULL) temp = temp->next;
-    }
-
-    node* cur = new node(root->data);
-    if(temp != NULL) temp->next = cur;
-    cur->next = right;
-
-    return (left == NULL) ? cur : left;
-}
-
 int main(){
     BST<int>* root = takeInput();
     printLevel(root);
     node* head = createSortedLL(root);
-    // 8 5 10 2 6 -1 -1 -1 -1 -1 7 -1 -1
     cout<<"Linked list: ";
     printLL(head);
-    cout<<endl;
-    cout<<"Linked list from pair: ";
-    node*head2 = createLL(root);
-    printLL(head2);
     delete root;
     delete head;
-    delete head2;
 }
