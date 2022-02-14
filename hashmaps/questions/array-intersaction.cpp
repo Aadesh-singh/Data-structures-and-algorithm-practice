f11c3e442954b4c4b5e1eb3f9b74fd55e42cebd9
Aadesh singh <aadeshsingh146@gmail.com>
2022-02-14 16:20:10 +0530

    Array Intersaction

--- hashmaps/questions/array-intersaction.cpp
+++ hashmaps/questions/array-intersaction.cpp
@@ -1,3 +1,10 @@
+// Problem Description:
+// You have been given two integer arrays/list(ARR1 and ARR2) of size N and M, respectively. You need to print their intersection; An intersection for this problem can be defined when both the arrays/lists contain a particular value or to put it in other words, when there is a common value that exists in both the arrays/lists.
+// Note :
+// Input arrays/lists can contain duplicate elements.
+
+// The intersection elements printed would be in the order they appear in the first sorted array/list(ARR1)
+
 #include<iostream>
 #include<unordered_map>
 using namespace std;
