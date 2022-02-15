84badb88cee0148af51daa3579552d9c65247a53
Aadesh singh <aadeshsingh146@gmail.com>
2022-02-15 13:11:23 +0530

    Traversing a Unordered_map

--- hashmaps/introduction.cpp
+++ hashmaps/introduction.cpp
@@ -32,4 +32,26 @@ int main(){
     } else {
         cout<<"Not Present"<<endl;
     }
+
+
+
+
+    // Traversing of Unordered map: 
+    unordered_map<string, int> m1;
+    m1.insert({"sss", 1});
+    m1.insert({"sss1", 2});
+    m1.insert({"sss2", 3});
+    m1.insert({"sss3", 4});
+    m1.insert({"sss4", 15});
+    unordered_map<string, int>:: iterator i = m1.begin();
+    while(i != m1.end()){
+        cout<<"Key: "<<i->first<<": "<<i->second<<endl;
+        i++;
+    }
+    m1.erase(m1.begin(), m1.end());
+    unordered_map<string, int>:: iterator it = m1.begin();
+    while(it != m1.end()){
+        cout<<"Key: "<<it->first<<": "<<it->second<<endl;
+        it++;
+    }
 }
