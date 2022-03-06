4736678d10b7819a94928cab61cc378dd17524c6
Aadesh singh <aadeshsingh146@gmail.com>
2022-03-06 18:23:28 +0530

    K Smallest numbers

--- priority-Queues/questions/k-smallest.cpp
+++ priority-Queues/questions/k-smallest.cpp
@@ -2,14 +2,33 @@
 #include<queue>
 using namespace std;
 
+// Aproach !  ==> (n-k)logn
+// void kSmallest(int* input, int n, int k){
+//     priority_queue<int> pq;
+
+//     for(int i=0; i<n; i++){
+//         pq.push(input[i]);
+//     }
+//     while(pq.size() != k){
+//         pq.pop();
+//     }
+//     while(!pq.empty()){
+//         cout<<pq.top()<<endl;
+//         pq.pop();
+//     }
+// }
+
+// Approach nlogk
 void kSmallest(int* input, int n, int k){
     priority_queue<int> pq;
-
-    for(int i=0; i<n; i++){
+    for(int i=0; i<k; i++){
         pq.push(input[i]);
     }
-    while(pq.size() != k){
-        pq.pop();
+    for(int i = k; i<n; i++){
+        if(input[i] < pq.top()){
+            pq.pop();
+            pq.push(input[i]);
+        }
     }
     while(!pq.empty()){
         cout<<pq.top()<<endl;
