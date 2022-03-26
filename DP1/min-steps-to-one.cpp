f187d5dcb952c94363a44c97bb28f9f9828678dc
Aadesh singh <aadeshsingh146@gmail.com>
2022-03-27 00:23:33 +0530

    Min steps to 1 with memoization

--- DP1/min-steps-to-one.cpp
+++ DP1/min-steps-to-one.cpp
@@ -2,9 +2,25 @@
 #include<climits>
 using namespace std;
 
+// without DP --> Memoization
+int minSteps2(int* arr, int n){
+    if(n <= 1){
+        return 0;
+    }
+    int x = (arr[n-1] != -1) ? arr[n-1] : minSteps2(arr,n-1);
+    int y = INT_MAX, z = INT_MAX;
+    if(n % 2 == 0){
+        y = (arr[n/2] != -1) ? arr[n/2] : minSteps2(arr,n/2);
+    }
+    if(n%3 == 0){
+        z = (arr[n/3] != -1) ? arr[n/3] : minSteps2(arr,n/3);
+    }
+    arr[n] = 1 + min(x, min(y, z));
 
+    return arr[n];   
+}
 
-// without DP
+// without DP -->BruteForce
 int minSteps(int n){
     if(n <= 1){
         return 0;
@@ -27,6 +43,11 @@ int main()
 {
     int n;
     cin>>n;
-    cout<<"Min Steps: "<<minSteps(n);
+    cout<<"Min Steps: "<<minSteps(n)<<endl;
+    int* arr = new int[n];
+    for(int i=0; i<n; i++) arr[i] = -1;
+    cout<<"Min Steps: "<<minSteps2(arr,n)<<endl;
+
+    delete [] arr;
     return 0;
 }
