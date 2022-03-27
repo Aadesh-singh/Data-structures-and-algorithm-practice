213393e304b442171013a14ccc45be7c11ebd85e
Aadesh singh <aadeshsingh146@gmail.com>
2022-03-27 15:42:16 +0530

    Min steps to one by using DP

--- DP1/min-steps-to-one.cpp
+++ DP1/min-steps-to-one.cpp
@@ -2,6 +2,33 @@
 #include<climits>
 using namespace std;
 
+// with DP
+int minSteps3(int* arr, int n){
+    arr[1] = 0;
+    for(int i=2; i<=n; i++){
+        int ans1 = INT_MAX, ans2 = INT_MAX, ans3 = INT_MAX;
+        if(i % 3 == 0){
+            if(arr[i/3] != -1){
+                ans1 = 1 + arr[i/3];
+            } else {
+                ans1 = 1 + 1 + arr[i/3];
+            }
+        }
+        if(i % 2 == 0){
+            if(arr[i/2] != -1){
+                ans2 = 1 + arr[i/2];
+            } else {
+                ans2 = 1 + 1 + arr[i/2];
+            }
+        }
+        if(arr[i-1] == -1) ans3 = 1 + 1 + arr[i-1];
+        else ans3 = 1 + arr[i - 1];
+
+        arr[i] = min(ans1, min(ans2, ans3));
+    }
+    return arr[n];
+}
+                                                                                                
 // without DP --> Memoization
 int minSteps2(int* arr, int n){
     if(n <= 1){
@@ -44,9 +71,11 @@ int main()
     int n;
     cin>>n;
     cout<<"Min Steps: "<<minSteps(n)<<endl;
-    int* arr = new int[n];
-    for(int i=0; i<n; i++) arr[i] = -1;
+    int* arr = new int[n + 1];
+    for(int i=0; i<=n; i++) arr[i] = -1;
     cout<<"Min Steps: "<<minSteps2(arr,n)<<endl;
+    for(int i=0; i<=n; i++) arr[i] = -1;
+    cout<<"Min Steps: "<<minSteps3(arr,n)<<endl;
 
     delete [] arr;
     return 0;
