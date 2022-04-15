1d60adb23c3d0b2cd2e9dc1fc12b891085a9cb0c
Aadesh singh <aadeshsingh146@gmail.com>
2022-04-15 12:33:10 +0530

    knapsack memoization

--- DP1/knapsack.cpp
+++ DP1/knapsack.cpp
@@ -17,9 +17,33 @@ int knapsack(int* weight, int* values, int n, int maxWeight){
     return max(x, y);
 }
 
+int knapsack_memo(int* weight, int* values, int n, int maxWeight, int* output){
+    if(n==0 || maxWeight == 0){
+        return 0;
+    }
+    if(output[n] != -1){
+        return output[n];
+    }
 
-int knapsack_memo(int* weight, int* values, int n, int maxHeight){
-    // 
+    if(weight[0] > maxWeight){
+        return knapsack(weight + 1, values + 1, n-1, maxWeight);
+    }
+
+    // Recusive calls
+    int x = knapsack(weight + 1, values + 1, n-1, maxWeight - weight[0]) + values[0];
+    int y = knapsack(weight + 1, values + 1, n-1, maxWeight);
+    output[n] = max(x, y);
+    return output[n];
+}
+
+int knapsack_memo(int* weight, int* values, int n, int maxWeight){
+    int* output = new int[n+1];
+    for(int i=0; i<=n; i++){
+        output[i] = -1;
+    }
+    int ans = knapsack_memo(weight, values, n, maxWeight, output);
+    delete [] output;
+    return ans;
 }
 
 int main()
@@ -38,6 +62,7 @@ int main()
     int maxWeight;
     cin>>maxWeight;
     cout<<knapsack(weight, values, n, maxWeight)<<endl;
+    cout<<knapsack_memo(weight, values, n, maxWeight)<<endl;
 
     delete [] weight;
     delete [] values;
