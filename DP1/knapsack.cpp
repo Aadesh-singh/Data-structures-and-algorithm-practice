19d5c4dee799f7cfc0521c89ff977cabdaf128aa
Aadesh singh <aadeshsingh146@gmail.com>
2022-04-15 13:36:20 +0530

    Knapsack dp

--- DP1/knapsack.cpp
+++ DP1/knapsack.cpp
@@ -46,6 +46,35 @@ int knapsack_memo(int* weight, int* values, int n, int maxWeight){
     return ans;
 }
 
+
+int knapsack_dp(int* weight, int* values, int n, int maxWeight){
+    //  int dp[n+1][maxWeight+1];
+     int** dp = new int*[n+1];
+     for(int i=0; i<=n; i++){
+         dp[i] = new int[maxWeight+1];
+     }
+	 for(int i =0 ; i<=n ;i++)
+		 { for(int j=0 ;j<=maxWeight ;j++)
+			 {
+ 			    if( i==0 || j== 0)
+ 			        dp[i][j]=0;
+ 			    else
+ 			    {  if(weight[i-1]>j)
+ 		  	         dp[i][j]  = dp[i-1][j];
+ 			        else
+ 			         dp[i][j]= max( values[i-1] + dp[i-1][j-weight[i-1]], dp[i-1][j]) ; 
+ 			    }
+			 }
+		 }
+    int ans =  dp[n][maxWeight];
+    for(int i=0; i<=n; i++){
+        delete [] dp[i];
+    }
+    delete [] dp;
+    return ans;
+}
+
+
 int main()
 {
     int n; cin>>n;
@@ -63,6 +92,7 @@ int main()
     cin>>maxWeight;
     cout<<knapsack(weight, values, n, maxWeight)<<endl;
     cout<<knapsack_memo(weight, values, n, maxWeight)<<endl;
+    cout<<knapsack_dp(weight, values, n, maxWeight)<<endl;
 
     delete [] weight;
     delete [] values;
