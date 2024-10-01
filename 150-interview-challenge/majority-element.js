607c63431e2847396464620636d0df349f8a3c6f
Aadesh singh <aadeshsingh146@gmail.com>
2024-10-01 12:19:01 +0530

    update majority element

--- 150-interview-challenge/majority-element.js
+++ 150-interview-challenge/majority-element.js
@@ -23,6 +23,7 @@ n == nums.length
 */
 
 // Sol - 1
+//Approach: Maintain a count map object and initial {-1: -1}, and loop in to that object tofind element with max count.
 var majorityElement = function (nums) {
   let count = { "-1": -1 };
   let max = -1;
@@ -46,6 +47,7 @@ var majorityElement = function (nums) {
 
 // sol - 2
 // this is with constant space and linear time
+// Approach: After sorting maintain two count finalCount and currentCount along with two pointers final(point to max count num) and cur(point to current counting num) and once cur change we update final if it is greater.
 
 var majorityEle = function (nums) {
   let finalCount = 0,
